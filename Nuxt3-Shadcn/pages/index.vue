bbd977acf2d06d5c95cd853546c371873396bcdb
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-12-08 15:00:00 +0300

    Feat:Set up And Set up Testing Is Done

---
+++ Nuxt3-Shadcn/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-yellow-400 font-extrabold">
+      I AM Also Checker Of Tailwind Css
+    </h1>
+    <Button>This Is Shadcn Button</Button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
